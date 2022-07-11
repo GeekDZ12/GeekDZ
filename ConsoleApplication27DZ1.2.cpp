aea0778dc721b6fa889c5adaab1ac704c6930950
wfrsssas <39248918+wfrsssas@users.noreply.github.com>
2022-07-11 20:38:17 +0600

    Add files via upload

---
+++ ConsoleApplication27DZ1.2.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+
+class Power{
+    double base = 2;
+    double sign = 16;
+//====================================================
+public:
+void setValues(double base, double sign) {
+    this->base = base; // указатели как в коспекте//
+    this->sign = sign;
+}
+double calculate();
+};
+//====================================================
+double Power::calculate() {
+    int s = sign;
+    double result = 1;
+    while (s) {
+        if (s % 2) {
+            result *= base;
+            s -= 1;
+        }
+        else {
+            base *= base;
+            s /= 2;
+        }
+    }
+    return result;
+}
+//========================================================
+    
+int main()
+{
+    Power p;
+    std::cout << p.calculate() << std::endl;
+    p.setValues(2, 10);
+    std::cout << p.calculate() << std::endl;
+}
+
